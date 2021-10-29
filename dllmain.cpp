22c6fb99a5c5dc4bb486c088ba1c19d8c2bfc162
RiceCum1 <89083789+RiceCum1@users.noreply.github.com>
2021-10-29 16:16:19 -0400

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -2,7 +2,7 @@
 #include "Engine.h"
 
 /*
- We will fill out outfit config to one from the store, and apply it to our own character 
+ We will fill the CurrentOutfitConfig variable or just take one from the store, and apply it to our own character 
  instead of the actor its applying to when previewing in the store. This can be customized 
  and improved a ton, even called without a hook (im lazy), but this is just an example.
 
